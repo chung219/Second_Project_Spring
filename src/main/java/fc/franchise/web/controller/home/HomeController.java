3c66ed8676e00effad0db8ff438452001d4d1b34
chung219 <a01039229675@gmail.com>
2023-07-17 15:07:03 +0900

    2nd project 0717

---
+++ src/main/java/fc/franchise/web/controller/home/HomeController.java
@@ -0,0 +1,22 @@
+package fc.franchise.web.controller.home;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class HomeController {
+
+    @GetMapping("/")
+    public String home () {
+        return "redirect:/main";
+    }
+
+    @GetMapping("/main")
+    public String main (){
+        return "main/main";
+    }
+
+}
